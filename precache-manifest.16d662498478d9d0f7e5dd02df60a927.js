b25d11f95f9dd1c6f9d30e27cd9cf11a9ab6faaf
Daniel <dslaurindo@gmail.com>
2020-08-17 22:59:14 -0300

    Updates

---
+++ precache-manifest.16d662498478d9d0f7e5dd02df60a927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b070da1783374e51bd1bbec27b566f",
+    "url": "/conversores-online/index.html"
+  },
+  {
+    "revision": "c9ddf311c1ba26a527ba",
+    "url": "/conversores-online/static/css/main.49360507.chunk.css"
+  },
+  {
+    "revision": "7eb38031137e6cb90c5c",
+    "url": "/conversores-online/static/js/2.7befe751.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/conversores-online/static/js/2.7befe751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ddf311c1ba26a527ba",
+    "url": "/conversores-online/static/js/main.6bbed4e1.chunk.js"
+  },
+  {
+    "revision": "692ec665f5e824b55274",
+    "url": "/conversores-online/static/js/runtime-main.a4fb1342.js"
+  }
+]);
