3322c4b8036b7d09c330ec1572f47a4cc160ac57
Daniel <dslaurindo@gmail.com>
2020-12-16 19:03:27 -0300

    Privacy

--- src/Privacy.jsx
+++ src/Privacy.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Privacy extends React.Component {
     render() {
         return (
-            <div className="grid five-column">
+            <div className="">
                 <h1>Política de Privacidade</h1>
                 <p><strong>1. Informações gerais</strong></p>
                 <p>A presente Política de Privacidade contém informações a respeito do modo como tratamos, total ou parcialmente, de forma automatizada ou não, os dados pessoais dos usuários que acessam nosso site. Seu objetivo é esclarecer os interessados acerca dos tipos de dados que são coletados, dos motivos da coleta e da forma como o usuário poderá atualizar, gerenciar ou excluir estas informações.</p>
@@ -11,20 +11,11 @@ class Privacy extends React.Component {
                 <p>Esta Política de Privacidade poderá ser atualizada em decorrência de eventual atualização normativa, razão pela qual se convida o usuário a consultar periodicamente esta seção.</p>
                 <p><br/><br/><strong>2. Direitos do usuário</strong></p>
                 <p>O site se compromete a cumprir as normas previstas no RGPD, em respeito aos seguintes princípios:</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão processados de forma lícita, leal e transparente (licitude, lealdade e transparência);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão coletados apenas para finalidades determinadas, explícitas e legítimas, não podendo ser tratados posteriormente de uma forma incompatível com essas finalidades (limitação das finalidades);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão coletados de forma adequada, pertinente e limitada às necessidades do objetivo para os quais eles são processados (minimização dos dados);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão exatos e atualizados sempre que necessário, de maneira que os dados inexatos sejam apagados ou retificados quando possível (exatidão);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão conservados de uma forma que permita a identificação dos titulares dos dados apenas durante o período necessário para as finalidades para as quais são tratados (limitação da conservação);</p>
-
-                <p><br/><br/><strong>2. Direitos do usuário</strong></p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão processados de forma lícita, leal e transparente (licitude, lealdade e transparência);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão coletados apenas para finalidades determinadas, explícitas e legítimas, não podendo ser tratados posteriormente de uma forma incompatível com essas finalidades (limitação das finalidades);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão coletados de forma adequada, pertinente e limitada às necessidades do objetivo para os quais eles são processados (minimização dos dados);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão exatos e atualizados sempre que necessário, de maneira que os dados inexatos sejam apagados ou retificados quando possível (exatidão);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão conservados de uma forma que permita a identificação dos titulares dos dados apenas durante o período necessário para as finalidades para as quais são tratados (limitação da conservação);</p>
-                <p style="padding-left: 30px;">- Os dados pessoais do usuário serão tratados de forma segura, protegidos do tratamento não autorizado ou ilícito e contra a sua perda, destruição ou danificação acidental, adotando as medidas técnicas ou organizativas adequadas (integridade e confidencialidade).</p>
-                
+                <p style={{"padding-left": "30px"}}>- Os dados pessoais do usuário serão processados de forma lícita, leal e transparente (licitude, lealdade e transparência);</p>
+                <p style={{"padding-left": "30px"}}>- Os dados pessoais do usuário serão coletados apenas para finalidades determinadas, explícitas e legítimas, não podendo ser tratados posteriormente de uma forma incompatível com essas finalidades (limitação das finalidades);</p>
+                <p style={{"padding-left": "30px"}}>- Os dados pessoais do usuário serão coletados de forma adequada, pertinente e limitada às necessidades do objetivo para os quais eles são processados (minimização dos dados);</p>
+                <p style={{"padding-left": "30px"}}>- Os dados pessoais do usuário serão exatos e atualizados sempre que necessário, de maneira que os dados inexatos sejam apagados ou retificados quando possível (exatidão);</p>
+                <p style={{"padding-left": "30px"}}>- Os dados pessoais do usuário serão conservados de uma forma que permita a identificação dos titulares dos dados apenas durante o período necessário para as finalidades para as quais são tratados (limitação da conservação);</p>
             </div>
         );
     }
